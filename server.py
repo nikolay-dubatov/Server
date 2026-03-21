c8f6a91e24cbed93f6f3e11ac03ba746e153fc60
Nikolay Dubatov <nikolay.dubatov@yandex.ru>
2026-03-21 18:08:51 +0700

    Create new endpoints structure

--- server.py
+++ server.py
@@ -3,14 +3,13 @@ from flask_cors import CORS
 import cv2
 from datetime import datetime
 
-app = Flask(__name__) # Создание веб-приложения
-CORS(app) # Настройка CORS
+app = Flask(__name__)
+CORS(app)
 
-# Источник видео (настройте под свой случай)
-VIDEO_SOURCE = 1  # 0 — встроенная камера; "rtsp://..." — IP‑камера; "video.mp4" — файл
+VIDEO_SOURCE = 1
 cap = cv2.VideoCapture(VIDEO_SOURCE)
 
-# ПРеобразования видео с камеры в MJPEG-поток
+# Преобразования видео с камеры в MJPEG-поток
 def generate_frames():
     """Генератор кадров из источника видео."""
     while True:
@@ -27,7 +26,7 @@ def generate_frames():
                    b"Content-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
 
 # Эндпоинт для видеопотока
-@app.route("/stream")
+@app.route("/camera/stream")
 def video_feed():
     """Эндпоинт для видеопотока."""
     return Response(generate_frames(),
@@ -41,23 +40,11 @@ def discover():
         "device_type": "test_server",
         "name": "Car Test Server", 
         "version": "1.0", 
-        "capabilities": [
-            "forward", "backward", "left", "right", 
-            "headlights_on", "headlights_off", 
-            "hazard_light_on", "hazard_light_off"
-        ]
     })
 
 # Эндпоинт телеметрии (данные с датчиков)
 @app.route("/telemetry")
 def get_telemetry():
-    """
-    Эндпоинт для получения данных с датчиков робота.
-    Вызывается клиентом каждые 2 секунды для обновления статуса.
-
-    В реальной реализации здесь будет чтение с физических датчиков.
-    Сейчас — тестовые данные.
-    """
     data = {
         "battery": 85,  # Уровень заряда батареи в процентах
         "obstacle_distance": 150  # Расстояние до препятствия в см (с ультразвукового датчика)
@@ -68,44 +55,83 @@ def get_telemetry():
         "data": data, # Основные данные
         "timestamp": now.strftime("%H:%M:%S") # Временная метка для синхронизации
     })
-
-@app.route("/<command>")
-def send_command(command):
-    """Универсальный обработчик команд управления."""
-#    print(f"Получена команда: {command}")
-    # Словарь команд
-    command_messages = {
-        "forward": "Движение вперёд",
-        "backward": "Движение назад",
-        "left": "Поворот влево",
-        "right": "Поворот вправо",
-        "stop": "Остановка",
-        "horn": "Гудок", 
-        "headlights_on": "Фары включены",
-        "headlights_off": "Фары выключены",
-        "hazard_lights_on": "Аварийная сигнализация включена",
-        "hazard_lights_off": "Аварийная сигнализация выключена", 
-        "parking_lights_on": "Габаритные огни включены", 
-        "parking_lights_off": "Габаритные огни выключены", 
-        "cam-up": "Камера вперёд", 
-        "cam-down": "Камера вниз", 
-        "cam-left": "Камера влево", 
-        "cam-right": "Камера вправо", 
-        "cam-stop": "Камера стоп"
-    }
-    # Вывод выполненной команды
-    if command in command_messages:
-        action = command_messages[command]
+@app.route('/car/<command>')
+def move_car(command):
+    commands = [
+        "stop", 
+        "left", 
+        "right", 
+        "forward", 
+        "backward"
+    ]
+    if command in commands:
+        return jsonify({
+            "status": "ok", 
+            "message": f"Команда {command} выполнена", 
+        })
     else:
         return jsonify({
             "status": "error", 
             "message": f"Неизвестная команда: {command}"
         }), 400
-    return jsonify({
-        "status": "ok", 
-        "command": command, 
-        "message": action
-    })
+@app.route("/car/<speed>")
+def select_speed(speed):
+    levels = [
+        "low", 
+        "medium", 
+        "high"
+    ]
+    if speed in levels: 
+        return jsonify({
+            "status": "ok", 
+            "message": f"Установлена скорость: {speed}"
+        })
+    else:
+        return jsonify({
+            "status": "error", 
+            "message": f"Неизвестная команда: {speed}"
+        }), 400
+@app.route("/camera/<command>")
+def control_camera(command):
+    commands = [
+        "cam-stop", 
+        "cam-down", 
+        "cam-up", 
+        "cam-left", 
+        "cam-right"
+    ]
+    if command in commands: 
+        return jsonify({
+            "status": "ok", 
+            "message": f"Команда {command} выполнена"
+        })
+    else:
+        return jsonify({
+            "status": "error", 
+            "message": f"Неизвестная команда: {command}"
+        }), 400
+@app.route('/light/<light_name>')
+def turn_lights(name):
+    names = [
+        "headlights", 
+        "hazard_lights", 
+        "parking_lights"
+    ]
+    states = [
+        "on", "off"
+    ]
+    state = request.args.get('state')
+    if name in names and state in states:
+        return jsonify({
+            "status": "ok", 
+            "message": f"{name}: {state}"
+        })
+    else: 
+        return jsonify({
+            "status":"error", 
+            "message": f"Неизвестная команда: {name}&{state=}"
+        }), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True, debug=False)
